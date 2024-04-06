198ee3f6a32354d738a39c352ebc85cebf52cbb5
Polina <pelageysha@yandex.ru>
2024-04-06 13:17:12 +0400

    add allure

--- tests/test_get_users_data.py
+++ tests/test_get_users_data.py
@@ -1,6 +1,7 @@
 import httpx
 from jsonschema import validate
 from core.contracts import USER_DATA_SCHEMA
+import allure
 
 BASE_URL = 'https://reqres.in'
 LIST_USERS = '/api/users?page=2'
@@ -9,17 +10,26 @@ EMAIL_ENDS = '@reqres.in'
 USER_NOT_FOUND = '/api/users/23'
 
 
+@allure.suite('Получение данных различных пользователей')
+@allure.title('Получение списка пользователей')
 def test_lists_users():
-    response = httpx.get(BASE_URL + LIST_USERS)
-    assert response.status_code == 200
+    with allure.step(f'отправляем запрос по адресу: {BASE_URL + LIST_USERS}'):
+        response = httpx.get(BASE_URL + LIST_USERS)
+    with allure.step('Проверяем код ответа'):
+        assert response.status_code == 200
 
     users_data = response.json()['data']
     for item in users_data:
-        validate(item, USER_DATA_SCHEMA)
-        assert item['email'].endswith(EMAIL_ENDS)
-        assert item['avatar'] == f'{BASE_URL}/img/faces/{item['id']}-image.jpg'
+        with allure.step(f'Проверяем структуру объекта с id: {item["id"]}'):
+            validate(item, USER_DATA_SCHEMA)
+        with allure.step('Проверяем e-mail'):
+            assert item['email'].endswith(EMAIL_ENDS)
+        with allure.step('Проверяем ссылку на аватар'):
+            assert item['avatar'] == f'{BASE_URL}/img/faces/{item['id']}-image.jpg'
 
 
+@allure.suite('Получение данных различных пользователей')
+@allure.title('Получение одного пользователя')
 def test_single_user():
     response = httpx.get(BASE_URL + SINGLE_USER)
     assert response.status_code == 200
@@ -30,6 +40,8 @@ def test_single_user():
     assert users_data['avatar'] == f'{BASE_URL}/img/faces/{users_data['id']}-image.jpg'
 
 
+@allure.suite('Получение данных различных пользователей')
+@allure.title('Получение несуществующего пользователя')
 def testuser_not_found():
     response = httpx.get(BASE_URL + USER_NOT_FOUND)
     assert response.status_code == 404
